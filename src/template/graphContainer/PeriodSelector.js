122579f087c77f24dfcb017109bcaab8cfa52b89
void <void@example.com>
2020-06-08 12:53:31 +0400

    Extract period selector

---
+++ src/template/graphContainer/PeriodSelector.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import { createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import { Period } from './Period';
+
+export function PeriodSelector(props) {
+  const THEME = createMuiTheme({
+    typography: {
+      fontFamily: "'Overpass', sans-serif;",
+      body1: {
+        fontSize: '10pt',
+      },
+    },
+  });
+
+  const StyledFormControlLabel = withStyles(() => ({
+    root: {
+      color: props.pointColor,
+    },
+  }))(FormControlLabel);
+
+  const StyledRadio = withStyles(() => ({
+    root: {
+      color: props.pointColor,
+    },
+  }))(Radio);
+
+  const [value, setValue] = React.useState(props.defaultPeriod);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <ThemeProvider theme={THEME}>
+      <FormControl component="fieldset">
+        <RadioGroup row aria-label="timeframe" name="timeframe" value={value} onChange={handleChange}>
+          <StyledFormControlLabel value={Period.M1} control={<StyledRadio size="small" />} label="1 MONTH" />
+          <StyledFormControlLabel value={Period.M3} control={<StyledRadio size="small" />} label="3 MONTHS" />
+          <StyledFormControlLabel value={Period.M6} control={<StyledRadio size="small" />} label="6 MONTHS" />
+        </RadioGroup>
+      </FormControl>
+    </ThemeProvider>
+  );
+}
