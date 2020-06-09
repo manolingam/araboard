2e6c1041a2bd40cdfed4365cabfa1f3e2b42cbfa
void <void@example.com>
2020-06-09 10:08:53 +0400

    Link to aragon.org

--- src/components/titleElement/TitleElement.js
+++ src/components/titleElement/TitleElement.js
@@ -4,27 +4,28 @@ import { ThemeContext } from '../../context/ThemeContext';
 import './TitleElement.css';
 
 class TitleElement extends Component {
-	static contextType = ThemeContext;
-	render() {
-		const { isLight, lightTheme, darkTheme } = this.context;
-		const theme = isLight ? lightTheme : darkTheme;
+  static contextType = ThemeContext;
+  render() {
+    const { isLight, lightTheme, darkTheme } = this.context;
+    const theme = isLight ? lightTheme : darkTheme;
 
-		return (
-			<div className='title-element'>
-				<div className='container-1'>
-					<h1>Aragon Network Metrics</h1>
-					<p style={{ color: theme.titleHelper }}>Visit Aragon.org</p>
-				</div>
-				<div className='container-2'>
-					<p style={{ color: theme.subTitle }}>
-						Lorem ipsum dolor sit amet, consectetuer adipiscing
-						elit, sed diam nonummy nibh euismod tincidunt ut laoreet
-						dolore magna aliquam erat volutpat.
-					</p>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="title-element">
+        <div className="container-1">
+          <h1>Aragon Network Metrics</h1>
+          <p style={{ color: theme.titleHelper }}>
+            <a href={'//aragon.org'}>Visit Aragon.org</a>
+          </p>
+        </div>
+        <div className="container-2">
+          <p style={{ color: theme.subTitle }}>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+            dolore magna aliquam erat volutpat.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TitleElement;
