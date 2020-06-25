e71c4a834cffae7720d83a3bbc8502def4716bb7
Manolingam N <manolingam1996@gmail.com>
2020-06-25 18:41:19 +0530

    added react helmet and og types

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { ThemeContextProvider } from './context/ThemeContext';
 import { ServicesProvider } from './context/ServicesContext';
 
+import { Helmet } from 'react-helmet';
+
 import { Main } from './pages/Main';
 
 import './App.css';
@@ -10,6 +12,19 @@ import './App.css';
 export function App() {
   return (
     <ThemeContextProvider>
+      <Helmet>
+        <title>Aragon Network Metrics</title>
+        <meta charSet="utf-8" />
+        <meta property="og:title" content="Aragon Network Metrics" />
+        <meta
+          name="description"
+          content="The Aragon Network is a digital jurisdiction that offers services to DAOs. It does so via linked protocols like Aragon Court. All these linked protocols have their tokens linked to ANT — the native token of the Aragon Network."
+        />
+        <meta
+          property="og:description"
+          content="The Aragon Network is a digital jurisdiction that offers services to DAOs. It does so via linked protocols like Aragon Court. All these linked protocols have their tokens linked to ANT — the native token of the Aragon Network."
+        />
+      </Helmet>
       <ServicesProvider>
         <Main />
       </ServicesProvider>
